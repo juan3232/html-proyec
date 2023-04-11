13520f5e93d505b4d71cb418c37b553dcf445015
Juan  Hernandez <72632179+juan3232@users.noreply.github.com>
2023-04-11 01:22:04 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,110 @@
+var sound = new Audio();
+sound.src = "./audios/A.mp3";
+
+var soundb = new Audio();
+soundb.src = "./audios/B.mp3";
+
+var soundc = new Audio();
+soundc.src = "./audios/C.mp3";
+
+var soundd = new Audio();
+soundd.src = "./audios/D.mp3";
+
+var sounde = new Audio();
+sounde.src = "./audios/E.mp3";
+
+var soundf = new Audio();
+soundf.src = "./audios/F.mp3";
+
+var soundg = new Audio();
+soundg.src = "./audios/G.mp3";
+
+var soundh = new Audio();
+soundh.src = "./audios/H.mp3";
+
+var soundi = new Audio();
+soundi.src = "./audios/I.mp3";
+
+var soundj = new Audio();
+soundj.src = "./audios/J.mp3";
+
+var soundk = new Audio();
+soundk.src = "./audios/K.mp3";
+
+var soundl = new Audio();
+soundl.src = "./audios/L.mp3";
+
+var soundm = new Audio();
+soundm.src = "./audios/M.mp3";
+
+var soundn = new Audio();
+soundn.src = "./audios/N.mp3";
+
+var soundñ = new Audio();
+soundñ.src = "./audios/ÑÑ.mp4";
+
+var soundo = new Audio();
+soundo.src = "./audios/O.mp3";
+
+var soundp = new Audio();
+soundp.src = "./audios/P.mp3";
+
+var soundq = new Audio();
+soundq.src = "./audios/Q.mp3";
+
+var soundr = new Audio();
+soundr.src = "./audios/R.mp3";
+
+var sounds = new Audio();
+sounds.src = "./audios/S.mp3";
+
+var soundt = new Audio();
+soundt.src = "./audios/T.mp3";
+
+var soundu = new Audio();
+soundu.src = "./audios/U.mp3";
+
+var soundv = new Audio();
+soundv.src = "./audios/V.mp3";
+
+var soundw = new Audio();
+soundw.src = "./audios/W.mp3";
+
+var soundx = new Audio();
+soundx.src = "./audios/X.mp3";
+
+var soundy = new Audio();
+soundy.src = "./audios/Y.mp3";
+
+var soundz = new Audio();
+soundz.src = "./audios/Z.mp3";
+
+
+
+
+var soundazul = new Audio();
+soundazul.src = "./audios/azul.mp3";
+
+var soundverde = new Audio();
+soundverde.src = "./audios/verde.mp3";
+
+var soundrojo = new Audio();
+soundrojo.src = "./audios/rojo.mp3";
+
+var soundamarillo = new Audio();
+soundamarillo.src = "./audios/amarillo.mp3";
+
+var soundcafe = new Audio();
+soundcafe.src = "./audios/cafe.mp3";
+
+var soundmorado = new Audio();
+soundmorado.src = "./audios/morado.mp3";
+
+var soundnaranja = new Audio();
+soundnaranja.src = "./audios/naranja.mp3";
+
+var soundcian = new Audio();
+soundcian.src = "./audios/cian.mp3";
+
+var soundrosa = new Audio();
+soundrosa.src = "./audios/rosa.mp3";
